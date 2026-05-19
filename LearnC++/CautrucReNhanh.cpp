9eb9ebd04ef685f1bf3a0f01bc4d5324621d384e
KySuBachKhoa <t.hoang1403@gmail.com>
2026-05-19 22:01:35 +0700

    Add files via upload

---
+++ LearnC++/CautrucReNhanh.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+// abs(x) : tri tuyet doi
+//pow(a, b) tinh a^b tra ve double
+//sqrt(n) tinh can bac 2 tra ve so double
+// round(a): lam tron so 
+    float a = 12.1231;
+    int b = round(a);
+    cout << b << endl;
+
+/* Cau truc re nhanh if else
+    if(condition){
+    // block of code if condition is true
+    } 
+*/
+    if (100> 50){
+        cout << "Xin Chao \n";
+    }
+}
