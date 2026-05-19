9c2e9c418be0359b43426c81cf6800d213e85392
KySuBachKhoa <t.hoang1403@gmail.com>
2026-05-19 16:48:16 +0700

    Add files via upload

---
+++ LearnC++/ToanTu.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a ;
+    long long b = 100000000; 
+    float c = 3.141;
+    double d = 5758235.34343;
+    char kitu = 'r'; // nhay don
+    bool ok = true ;
+    cout << b << endl;  
+    cout << "Nhap gia tri cho a :";
+    cin >> a; 
+    cout << "Gia tri cua a la: " << a << endl ;
+    return 0;
+}
+// Dấu '=' được gọi là toán từ gán (Assigment Operator)
+// Toán tử toán học
