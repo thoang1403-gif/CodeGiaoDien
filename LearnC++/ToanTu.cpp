4342d3760cd8a8f6d621c4c5f550b385ace89705
KySuBachKhoa <t.hoang1403@gmail.com>
2026-05-19 16:52:20 +0700

    Add files via upload

--- LearnC++/ToanTu.cpp
+++ LearnC++/ToanTu.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
+// Dấu '=' được gọi là toán từ gán (Assigment Operator)
+// Toán tử toán học: + - * / %  
+// Toan tu so sanh : >, >=, <, <=, ==, !=
+// Toan tu logic And or not
+// AND : &&
+// Or: ||
+// Not: !
 int main(){
     int a ;
-    long long b = 100000000; 
+    long long b = 100; 
     float c = 3.141;
     double d = 5758235.34343;
     char kitu = 'r'; // nhay don
@@ -13,7 +19,8 @@ int main(){
     cout << "Nhap gia tri cho a :";
     cin >> a; 
     cout << "Gia tri cua a la: " << a << endl ;
+    float thuong = 1.0*a / b; // (float)a/b or a/(float)b
+    cout << thuong << endl;
     return 0;
 }
-// Dấu '=' được gọi là toán từ gán (Assigment Operator)
-// Toán tử toán học
+
